ea6c7d07426d4e74e2df835d50a679d6f2688b1a
Ecclesiology <JMMatthews@Protonmail.com>
2020-04-22 17:10:43 -0500

    Add columns to grid container

--- drawing.js
+++ drawing.js
@@ -3,3 +3,8 @@ const gridContainer = document.querySelector(".gridContainer")
 const rows = document.createElement("div");
   rows.classList.add("Rows");
   gridContainer.appendChild(rows);
+  
+const columns = document.createElement("div");
+  columns.classList.add("Columns");
+  gridContainer.appendChild(columns);
+  
